47b1efbab771dc1ae26a95744b6c94b3b2c0b77b
nikolanekudova <nikolka.nekudova@seznam.cz>
2023-12-26 19:29:38 +0100

    feat(): initial commit

---
+++ client/pages/index.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useEffect, useState } from "react";
+
+function index() {
+  const [message, setMessage] = useState("Loading");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/home")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMessage(data.message);
+      });
+  }, []);
+
+  return <div>{message}</div>;
+}
+
+export default index;
